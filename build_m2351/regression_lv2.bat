8719fdb2798ec036ba7b827bde6269071dc8c130
MS20 WSChang0 <wschang0@nuvoton.com>
2019-11-21 19:48:18 +0800

    update regression lv2 build command for M2351

--- build_m2351/regression_lv2.bat
+++ build_m2351/regression_lv2.bat
@@ -1,6 +1,6 @@
 rm -f -r ../build
 mkdir ..\build
 cd ..\build
-cmake  ../ -G"Unix Makefiles" -DPROJ_CONFIG="D:/MCU/TF-M/trusted-firmware-m/ConfigRegressionIPCTfmLevel2.cmake" -DTARGET_PLATFORM=M2351 -DCOMPILER=ARMCLANG -DCMAKE_BUILD_TYPE=Debug -DMBEDTLS_DEBUG=ON -DBL2=False -DBUILD_PLAT_TEST=False
+cmake ../ -G"Unix Makefiles" -DPROJ_CONFIG="D:/MCU/TF-M/trusted-firmware-m/configs/ConfigRegressionIPCTfmLevel2.cmake" -DTARGET_PLATFORM=M2351 -DCOMPILER=ARMCLANG -DCMAKE_BUILD_TYPE=DEBUG -DITS_RAM_FS=OFF -DBL2=False -DBUILD_PLAT_TEST=False -DBUILD_DWARF_VERSION=2 -DMBEDTLS_DEBUG=ON
 make
 
