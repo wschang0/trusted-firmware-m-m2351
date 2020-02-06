ae238c81220d6151b423ed8d57c40c574cd3e8d9
WSChang0 <wschang0@nuvoton.com>
2020-02-06 11:07:29 +0800

    Use hardware TRNG to generate boot seed

--- platform/ext/target/Nuvoton/M2351/dummy_boot_seed.c
+++ platform/ext/target/Nuvoton/M2351/dummy_boot_seed.c
@@ -15,6 +15,7 @@
  */
 
 #include "platform/include/tfm_plat_boot_seed.h"
+#include "NuMicro.h"
 
 /*!
  * \def BOOT_SEED
@@ -26,7 +27,7 @@
                     0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, \
                     0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF
 
-static const uint8_t boot_seed[BOOT_SEED_SIZE] = {BOOT_SEED};
+//static const uint8_t boot_seed[BOOT_SEED_SIZE] = {BOOT_SEED};
 
 enum tfm_plat_err_t tfm_plat_get_boot_seed(uint32_t size, uint8_t *buf)
 {
@@ -36,18 +37,33 @@ enum tfm_plat_err_t tfm_plat_get_boot_seed(uint32_t size, uint8_t *buf)
      */
 
     uint32_t i;
-    uint8_t *p_dst = buf;
-    const uint8_t *p_src = boot_seed;
+	uint32_t u32Reg;
 
     if (size != BOOT_SEED_SIZE) {
         return TFM_PLAT_ERR_SYSTEM_ERR;
     }
 
-    for (i = size; i > 0; i--) {
-        *p_dst = *p_src;
-        p_src++;
-        p_dst++;
-    }
+	/* Basic Configuration */
+	CLK->APBCLK0 |= CLK_APBCLK0_RTCCKEN_Msk;
+	CLK->APBCLK1 |= CLK_APBCLK1_TRNGCKEN_Msk;
+	RTC->LXTCTL |= RTC_LXTCTL_LIRC32KEN_Msk | RTC_LXTCTL_C32KS_Msk;
+
+	SYS->IPRST1 |= SYS_IPRST1_TRNGRST_Msk;
+	SYS->IPRST1 ^= SYS_IPRST1_TRNGRST_Msk;
+
+	TRNG->ACT |= TRNG_ACT_ACT_Msk;
+	/* Waiting for ready */
+	while ((TRNG->CTL & TRNG_CTL_READY_Msk) == 0);
+
+	TRNG->CTL = (9 << TRNG_CTL_CLKP_Pos);
+
+	u32Reg = TRNG->CTL;
+	for (i = 0;i < size;i++)
+	{
+		TRNG->CTL = TRNG_CTL_TRNGEN_Msk | u32Reg;
+		while ((TRNG->CTL&TRNG_CTL_DVIF_Msk) == 0);
+		buf[i] = TRNG->DATA;
+	}
 
     return TFM_PLAT_ERR_SUCCESS;
 }
