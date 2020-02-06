253f6a9dbb5b1d272b257505f70fe139190893bf
WSChang0 <wschang0@nuvoton.com>
2020-02-06 11:10:51 +0800

    Update lifecycle statuc according to lock status

--- platform/ext/target/Nuvoton/M2351/attest_hal.c
+++ platform/ext/target/Nuvoton/M2351/attest_hal.c
@@ -7,16 +7,35 @@
 
 #include "platform/include/tfm_attest_hal.h"
 #include <stdint.h>
+#include "NuMicro.h"
 
 /* Example verification service URL for initial attestation token */
-static const char verification_service_url[] = "www.trustedfirmware.org";
+static const char verification_service_url[] = "www.nuvoton.com";
 
 /* Example profile definition document for initial attestation token */
 static const char attestation_profile_definition[] = "psa-tfm-profile-1.md";
 
 enum tfm_security_lifecycle_t tfm_attest_hal_get_security_lifecycle(void)
 {
-    return TFM_SLC_SECURED;
+    uint32_t u32SCRLOCK, u32ARLOCK;
+    /* check the lock status */
+    FMC->ISPCTL |= FMC_ISPCTL_ISPEN_Msk;
+    FMC->ISPADDR = FMC_SCRLOCK_BASE;
+    FMC->ISPCMD = FMC_ISPCMD_READ;
+    FMC->ISPTRG = FMC_ISPTRG_ISPGO_Msk;
+    while(FMC->ISPTRG){}
+    u32SCRLOCK = FMC->ISPDAT;
+    FMC->ISPADDR = FMC_ARLOCK_BASE;
+    FMC->ISPCMD = FMC_ISPCMD_READ;
+    FMC->ISPTRG = FMC_ISPTRG_ISPGO_Msk;
+    while(FMC->ISPTRG) {}
+    u32ARLOCK = FMC->ISPDAT;
+
+    if(((FMC->ISPDAT&0xff) != 0x5a) || ((FMC->ISPDAT&0xff) != 0x5a))
+        return TFM_SLC_SECURED;
+    else
+        return TFM_SLC_UNKNOWN;
+
 }
 
 const char *
